89270a605d73c2285ee90318d951564aaa6f2480
Tim Nimets <tymnim@gmail.com>
2021-02-25 01:07:15 -0500

    added getExpenseSheet route

---
+++ server/routes/getExpenseSheet.js
@@ -0,0 +1,32 @@
+const {
+  retrieveDataFrom,
+  idFromString
+} = require("../src/utils.js")
+
+exports.getExpenseSheet = {
+  type: "get",
+  path: "/sheets/:id",
+  authNeeded: true,
+  callback: async function getExpenseSheet(req, res) {
+    try {
+      const username = req.username
+      // TODO: some security and restrictions so people cannot fetch something they do not have access to
+      const doc = global.db.collection("sheets").findOne({ _id: idFromString(req.params.id) })
+      if (doc) {
+        res.status(200)
+        res.send({ message: "OK", expenseSheet: doc })
+        res.end()
+        return
+      }
+      res.status(404)
+      res.send({ message: "Not Found" })
+      res.end()
+    }
+    catch (err) {
+      console.error(err)
+      res.status(500)
+      res.send({ error: "Internal server error" })
+      res.end()
+    }
+  }
+}
--- server/routes/getExpenseSheets.js
+++ server/routes/getExpenseSheets.js
@@ -8,14 +8,13 @@ exports.getExpenseSheets = {
   authNeeded: true,
   callback: async function getExpenseSheets(req, res) {
     try {
-      
-    const username = req.username
-    const cursor = global.db.collection("sheets").find({createdBy: username})
-    const expenseSheets = await cursor.toArray();
-      
-    res.status(200)
-    res.send({ message: "Expense sheet was received", expenseSheets: expenseSheets })
-    res.end()
+      const username = req.username
+      const cursor = global.db.collection("sheets").find({createdBy: username})
+      const expenseSheets = await cursor.toArray();
+
+      res.status(200)
+      res.send({ message: "Expense sheet was received", expenseSheets: expenseSheets })
+      res.end()
     }
     catch (err) {
       console.error(err)
