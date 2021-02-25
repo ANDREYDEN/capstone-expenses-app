d8b480548d5cbef06e8828c7e63e6e31e77df800
Tim Nimets <tymnim@gmail.com>
2021-02-25 01:08:52 -0500

    added client api call to get a single expense sheet

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -12,13 +12,16 @@ export const signup = (username, password) => {
 	return api.post("/signup", { username, password })
 }
 export const createNewExpenseSheet = () => {
-  return api.post("/sheet/new")
+  return api.post("/sheets/new")
 }
 export const retrieveExpenseSheet = () => {
-  return api.get("/retrieveSheet")
+  return api.get("/sheets")
 }
 export const authentificate = () => {
   return api.get("/authentificate")
+}
+export const getSheetById = (id) => {
+  return api.get(`/sheets/${id}`)
 }
 
 const apis = {
