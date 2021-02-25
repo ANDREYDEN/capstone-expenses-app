51e9a169f93f58de6e9fb799831dbbfbed3099bd
Tim Nimets <tymnim@gmail.com>
2021-02-25 00:04:17 -0500

    some formating and comment for expenceSheetList.jsx

--- client/src/components/expenceSheetList.jsx
+++ client/src/components/expenceSheetList.jsx
@@ -1,26 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createNewExpenceSheet } from '../api/index.js'
-import { retrieveExpenseSheet }  from '../api/index.js'
-import axios from 'axios'
-
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { createNewExpenceSheet, retrieveExpenseSheet } from "../api/index.js"
+import axios from "axios"
 
 export default class ExpenceSheetList extends React.Component {
-
   constructor(props) {
     super(props)
     this.state = {
       sheets: []
     }
   }
-
-  componentDidMount() {
-  }
-
   addNewExpenceSheet() {
+    // TODO: some nitifcation if success
+    // TODO: redirect if successful
     createNewExpenceSheet().then(result => console.log(result)).catch(err => console.error(err))
   }
   retrieveExpenseSheets() {
+    // TODO: this should be executed on start
     retrieveExpenseSheet().then((res) => {
       this.setState({ sheets: res.data.expenseSheets })
     }).catch(err => console.error(err))
@@ -28,7 +25,11 @@ export default class ExpenceSheetList extends React.Component {
 
   render() {
     const items = this.state.sheets.map((sheet, index) => { 
-      return <li key={index}>{index + 1}.{sheet._id}</li>
+      return (
+        <li key={index}>
+          <a href="/sheets/{sheet._id}"></a>{index + 1}.{sheet._id}
+        </li>
+      )
     });
 
     const groupId = "6036d85f7e0fff3b44e09391";
