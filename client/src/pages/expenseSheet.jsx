5cba010229ff7e2a2fe7adbf5dca6b53851aa452
Tim Nimets <tymnim@gmail.com>
2021-02-26 20:40:58 -0500

    getting routes to work properly; addeing item not found case

--- client/src/pages/expenseSheet.jsx
+++ client/src/pages/expenseSheet.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom"
 import axios from "axios"
 import { useParams } from "react-router-dom"
 import { getSheetById } from "../api/index.js"
+import Spinner from "../components/spinner.jsx"
 
 export default class ExpenseSheetList extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      confirmedSheetWithServer: false,
       sheet: { 
         name: "",
         store: "",
@@ -28,31 +30,49 @@ export default class ExpenseSheetList extends React.Component {
   componentDidMount() {
     getSheetById(this.sheetId).then(res => {
       console.log(res)
+      this.setState({
+        serverConfirmed: true,
+        sheet: res.data.expenseSheet
+      })
     }).catch(err => {
       // TODO: user notifications
+      this.setState({
+        serverConfirmed: true,
+        sheet: null
+      })
       console.log(err)
     })
   }
 
   render() {
+    if (this.state.serverConfirmed) {
+      if (this.state.sheet) {
+        const sheet = this.state.sheet
+        return (
+          <div className="expense-sheet-container">
+            <label htmlFor="">Expense Sheet Name:</label>
+            <input type="text" name="" ref={this.sheetName} defaultValue={sheet.name}/>
+            <br/>
+            <label htmlFor="">Store Name:</label>
+            <input type="text" name="" ref={this.storeName} defaultValue={sheet.store}/>
+            <br/>
+            <label htmlFor="">Tax Included:</label>
+            <input type="checkbox" name="" ref={this.taxIncluded}/>
+            <br/>
+            <span>Date: {sheet.createdAt}</span>
+            <br/>
+            <span>Created By: {sheet.createdBy}</span>
 
-    const sheet = this.state.sheet
-    return (
-      <div className="expense-sheet-container">
-        <label htmlFor="">Expense Sheet Name:</label>
-        <input type="text" name="" ref={this.sheetName} defaultValue={sheet.name}/>
-        <br/>
-        <label htmlFor="">Store Name:</label>
-        <input type="text" name="" ref={this.storeName} defaultValue={sheet.store}/>
-        <br/>
-        <label htmlFor="">Tax Included:</label>
-        <input type="checkbox" name="" ref={this.taxIncluded}/>
-        <br/>
-        <span>Date: {sheet.createdAt}</span>
-        <br/>
-        <span>Created By: {sheet.createdBy}</span>
+          </div>
+        )
+      }
+      return (
+        <div className="expense-sheet-container">
+          Item is not found.
+        </div>
+      )
+    }
+    return <Spinner />
 
-      </div>
-    )
   }
 }
