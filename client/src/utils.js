06bc438f8069a5afe51e0a444cb661dec2d58cd6
Tim Nimets <tymnim@gmail.com>
2021-02-26 23:06:00 -0500

    added debounce function to client side utils

--- client/src/utils.js
+++ client/src/utils.js
@@ -6,12 +6,12 @@
  *
  * @return {Object} Returns the newly created object that has the fields that present in object one and 2 but different and that are .
  */
-function differ(obj1, obj2) {
+export function differ(obj1, obj2) {
   const obj1Keys = Object.keys(obj1)
   const obj2Keys = Object.keys(obj2)
   const diffFields = obj1Keys.map(key => {
     if (JSON.stringify(obj1[key]) !== JSON.stringify(obj2[key])) {
-      return [key, obj2[key]];
+      return [key, obj2[key]]
     }
     return []
   })
@@ -26,6 +26,19 @@ function differ(obj1, obj2) {
   }, {})
 }
 
+export function debounce(func, wait) {
+  let timeout
+  return (...args) => {
+    const later = () => {
+      timeout = null
+      func(...args)
+    }
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+  }
+}
+
 export default {
-  differ
+  differ,
+  debounce
 }
