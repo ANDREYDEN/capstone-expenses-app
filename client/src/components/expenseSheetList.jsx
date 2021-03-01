7a661a85ba57bc57543993ddedbc70f9fd9746e0
Tim Nimets <tymnim@gmail.com>
2021-02-28 22:13:39 -0500

    added short summary to each fo the expense sheets

--- client/src/components/expenseSheetList.jsx
+++ client/src/components/expenseSheetList.jsx
@@ -10,6 +10,7 @@ export default class ExpenseSheetList extends React.Component {
     this.state = {
       sheets: []
     }
+    this.groupId = "6036d85f7e0fff3b44e09391";
   }
   addNewExpenseSheet() {
     // TODO: some nitifcation if success
@@ -24,16 +25,39 @@ export default class ExpenseSheetList extends React.Component {
   }
 
   render() {
-    const items = this.state.sheets.map((sheet, index) => { 
+    const user = JSON.parse(localStorage.getItem("user"))
+    const summary = this.state.sheets.reduce((memo, sheet) => {
+      //TODO: filter only sheets that all users marked as completed
+      const userOwes = sheet.entries.reduce((sum, entry) => {
+        if (entry.userCheckedIds[user._id]) {
+          sum += (entry.price || 0) / Object.keys(entry.userCheckedIds).filter(key => entry.userCheckedIds[key]).length
+        }
+        return sum
+      }, 0)
+      let userOwns = 0
+      // TODO: must be changed to id and the same on the backend
+      // TODO: consider adding a new filed something like createdByName so we do not have to lookup
+      if (sheet.createdBy === user.name) {
+        userOwns = sheet.entries.reduce((sum, entry) => {
+          sum += entry.price || 0
+          return sum
+        }, 0)
+      }
+      memo[sheet._id] = userOwns - userOwes
+      return memo
+    }, {})
+
+    const items = this.state.sheets.map((sheet, index) => {
       return (
         <li key={index}>
-          <a href={"/sheets/" + sheet._id}>{index + 1}.{sheet._id}</a>
+          <a href={"/sheets/" + sheet._id}>
+            {index + 1}.
+            {`${sheet.name} by ${sheet.createdBy}`}
+            {`${summary[sheet._id] > 0 ? "you own:" : "you owe:"} ${Math.abs(summary[sheet._id])}`}
+          </a>
         </li>
       )
     });
-
-    const groupId = "6036d85f7e0fff3b44e09391";
-
     return (
       <div className="expense-sheet-container">
         <ul>
@@ -41,7 +65,7 @@ export default class ExpenseSheetList extends React.Component {
           <li>
             <button onClick={this.addNewExpenseSheet}>Add New One</button>
             <br/>
-            <button onClick={this.retrieveExpenseSheets.bind(this)}>Open Spreadsheet</button>
+            <button onClick={this.retrieveExpenseSheets.bind(this)}>Show Spreadsheets</button>
 
           </li>
         </ul>
