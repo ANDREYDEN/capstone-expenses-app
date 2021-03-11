4d5dfd92326a18caff9647d5ace10c1c4d3fd922
Tim Nimets <tymnim@gmail.com>
2021-03-11 18:13:07 -0500

    added conditional rendering of google logout button

--- client/src/pages/main.jsx
+++ client/src/pages/main.jsx
@@ -10,12 +10,18 @@ export default class Main extends React.Component {
   }
 
   render() {
+    const signedWithGoogle = document.cookie.split("; ").reduce((memo, cookie) => {
+      const [name, value] = cookie.split("=")
+      return memo || (name === "tokenId" && value)
+    }, false)
+    const googleLogout = <GoogleSignOut  logout={this.props.logout}/>
+    const logoutButton = signedWithGoogle ? googleLogout : <div></div>
     return (
       <main>
         <Router>
           <Route exact path="/home">
             <ExpenseSheetList />
-            <GoogleSignOut  logout={this.props.logout}/>
+            {logoutButton}
           </Route>
           <Route exact path="/sheets/:id" component={ExpenseSheet} />
         </Router>
