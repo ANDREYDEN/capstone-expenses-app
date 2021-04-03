16f8e87cb708fdf10af02bcc5a36dfcbae03a5af
Tim Nimets <tymnim@gmail.com>
2021-04-03 14:50:59 -0400

    added joinGroup page

---
+++ client/src/components/joinGroup.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { Redirect } from "react-router-dom"
+import { joinGroup } from "../api/index.js"
+
+export default class JoinGroup extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log(props)
+    this.state = {
+      redirect: null,
+      error: null
+    }
+  }
+  componentDidMount() {
+    joinGroup(this.props.match.params.id).then(res => {
+      const newUser = res.data.user
+      localStorage.setItem("user", JSON.stringify(newUser))
+      this.setState({ redirect: `/home/${this.props.match.params.id}` })
+    }).catch((err) => {
+      this.setState({ error: err })
+    })
+  }
+  render() {
+    const redirect = this.state.redirect
+    return (redirect ? <Redirect to={redirect} /> : <h2 className="error">{this.state.error?.name} {this.state.error?.message}</h2>)
+  }
+}
