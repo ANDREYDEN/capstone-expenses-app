7cf289633a3d693eb1c89472508ce12bf0d584d7
Tim Nimets <tymnim@gmail.com>
2021-04-03 12:55:38 -0400

    implemented addMemeber component

--- client/src/components/groupManager.jsx
+++ client/src/components/groupManager.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import AddGroup from "./addGroup.jsx"
+import AddMember from "./addMember.jsx"
 import GroupDropDown from "./groupDropDown.jsx"
 import "../styles/groupManager.scss"
 
@@ -11,6 +12,7 @@ export default class GroupManager extends React.Component {
     super(props)
     this.state = {
       showGroupCreation: false,
+      showAddMembers: false,
       selectedGroupId: props.groupId || null
     }
   }
@@ -36,10 +38,22 @@ export default class GroupManager extends React.Component {
       showGroupCreation: false
     })
   }
+  showAddMembers() {
+    this.setState({
+      showAddMembers: true
+    })
+  }
+  hideAddMembers() {
+    this.setState({
+      showAddMembers: false
+    })
+  }
   render() {
     const groups = this.globalState.get("groups") || []
-    const groupDropDown = groups.length ? <GroupDropDown onGroupChange={this.props.onGroupChange} groups={groups} selectedGroupId={this.props.groupId}/> : null
+    const selectedGroupId = this.props.selectedGroupId || this.globalState.get("selectedGroupId")
+    const groupDropDown = groups.length ? <GroupDropDown onGroupChange={this.props.onGroupChange} groups={groups} selectedGroupId={selectedGroupId}/> : null
     const addGroup = this.state.showGroupCreation ? <AddGroup onSuccess={this.hideGroupCreation.bind(this)} /> : null
+    const addMembers = this.state.showAddMembers ? <AddMember groupId={selectedGroupId} onSuccess={this.hideAddMembers.bind(this)} /> : null
     const groupDict = (groups || []).reduce((memo, group) => {
       memo[group._id] = group
       return memo
@@ -50,6 +64,9 @@ export default class GroupManager extends React.Component {
           // NOTE: this is temporary. To be changed
           <button onClick={this.showGroupCreation.bind(this)}>New Group</button>
           {addGroup}
+          // NOTE: this is temporary. To be changed
+          <button onClick={this.showAddMembers.bind(this)}>Add Member</button>
+          {addMembers}
         </div>
         <div className="header">
           {groupDropDown}
