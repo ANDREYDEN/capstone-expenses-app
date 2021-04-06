cca2d88da6033257ccd02255212e93e3d9ebae24
Ben Powers <ben_powers94@hotmail.com>
2021-04-06 14:42:08 -0400

    fixed indentations

--- client/src/pages/payBalances.jsx
+++ client/src/pages/payBalances.jsx
@@ -6,48 +6,48 @@ import "../styles/payBalances.scss"
 import { FaArrowLeft } from "react-icons/fa"
 
 export default class PayBalances extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-        }
-    }
-    
-    componentDidMount() {
-    }
+  constructor(props) {
+      super(props)
+      this.state = {
+      }
+  }
+  
+  componentDidMount() {
+  }
 
-    render() {
+  render() {
     const { balance } = this.props.location.state
-      return (
-        <div className = "pay-balances-container">
-          <Link to={{ pathname: '/balances'}}>
-            <h2>
+    return (
+      <div className = "pay-balances-container">
+        <Link to={{ pathname: '/balances'}}>
+          <h2>
             <FaArrowLeft/>
-            </h2>
-          </Link>
-          <h2>Pay Balance</h2>
-          <h4>How much are you paying?</h4>
+          </h2>
+        </Link>
+        <h2>Pay Balance</h2>
+        <h4>How much are you paying?</h4>
         <div className="image-and-name">
           <div className = "image-container">
             <img src="https://s3.amazonaws.com/pixpa.com/com/articles/1525891879-76924-tanja-heffner-584866-unsplashjpg.png" alt="Logo" />
           </div>
           <h4>{balance.name} </h4>
         </div>
-      <li className="user">
-      <Link to={{
-        pathname: '/payBalanceFull',
-        state: {
-          balance
-        }}}>
-        <span className = "user-name">
-          Pay Full Balance 
-        </span>
-        <span className = "pull-right">
-          ${balance.userOwes.sum}
-        </span>
-      </Link>
-      </li>
+        <div className="user">
+          <Link to={{
+            pathname: '/payBalanceFull',
+            state: {
+              balance
+          }}}>
+            <span className = "user-name">
+              Pay Full Balance 
+            </span>
+            <span className = "pull-right">
+              ${balance.userOwes.sum}
+            </span>
+          </Link>
+        </div>
       </div>
     )
-}
+  } 
 }
 
