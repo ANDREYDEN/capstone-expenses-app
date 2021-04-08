e0cd575bd3d2044ef0b44882447114904ce297be
Tim Nimets <tymnim@gmail.com>
2021-04-08 18:25:46 -0400

    added reusable avatar component

---
+++ client/src/components/avatar.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "../styles/avatar.scss"
+
+export default class PayBalances extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const user = this.props.user
+    if (!user) {
+       return null
+    }
+    if (user.imageUrl) {
+      return null
+    }
+    const initials = user.name.split(" ").map((name, index) => <span key={index}>{name[0].toUpperCase()}</span>)
+    return (
+      <div className="avatar" style={{background: user.color}}>
+        {initials}
+      </div>
+    )
+  }
+}
