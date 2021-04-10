5dcae716c37378ea0033fed7f9c01247c50e1119
Tim Nimets <tymnim@gmail.com>
2021-04-09 23:55:54 -0400

    added owe-own calculating for the user

--- client/src/components/expenseSheetList.jsx
+++ client/src/components/expenseSheetList.jsx
@@ -7,10 +7,7 @@ import "../styles/expenseSheetList.scss"
 export default class ExpenseSheetList extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      sheets: []
-    }
-    this.groupId = this.props.groupId
+    this.groupId = null
   }
 
   componentDidMount() {
@@ -19,8 +16,9 @@ export default class ExpenseSheetList extends React.Component {
   componentDidUpdate() {
     if (this.groupId !== this.props.groupId) {
       this.groupId = this.props.groupId
-      retrieveExpenseSheets(this.groupId).then((res) => {
-        this.setState({ sheets: res.data.expenseSheets })
+      retrieveExpenseSheets(this.props.groupId).then((res) => {
+        console.log(res.data.expenseSheets)
+        this.globalState.set({ sheets: res.data.expenseSheets })
       }).catch(err => console.error(err))
     }
   }
@@ -28,8 +26,8 @@ export default class ExpenseSheetList extends React.Component {
   addNewExpenseSheet() {
     // TODO: some nitifcation if success
     // TODO: redirect if successful
-    createNewExpenseSheet(this.groupId).then(result => {
-      const sheets = this.state.sheets
+    createNewExpenseSheet(this.props.groupId).then(result => {
+      const sheets = this.globalState.get("sheets")
       sheets.push(result.data.newSheet)
       this.setState({ sheets })
     }).catch(err => console.error(err))
@@ -37,29 +35,29 @@ export default class ExpenseSheetList extends React.Component {
 
   render() {
     const user = JSON.parse(localStorage.getItem("user"))
-    const summary = this.state.sheets.reduce((memo, sheet) => {
-      //TODO: filter only sheets that all users marked as completed
-      const userOwes = sheet.usersPaidIds[user._id] ? 0 : sheet.entries.reduce((sum, entry) => {
-        if (entry.userCheckedIds[user._id]) {
-          sum += (entry.price || 0) / Object.keys(entry.userCheckedIds).filter(key => entry.userCheckedIds[key]).length
-        }
-        return sum
-      }, 0)
-      let userOwns = 0
-      // TODO: must be changed to id and the same on the backend
-      // TODO: consider adding a new filed something like createdByName so we do not have to lookup
-      if (sheet.createdBy === user.name) {
-        userOwns = sheet.entries.reduce((sum, entry) => {
-          sum += entry.price || 0
-          return sum
-        }, 0)
-      }
-      memo[sheet._id] = userOwns - userOwes
-      memo.total += userOwns - userOwes
-      return memo
-    }, { total: 0 })
+    // const summary = this.state.sheets.reduce((memo, sheet) => {
+    //   //TODO: filter only sheets that all users marked as completed
+    //   const userOwes = sheet.usersPaidIds[user._id] ? 0 : sheet.entries.reduce((sum, entry) => {
+    //     if (entry.userCheckedIds[user._id]) {
+    //       sum += (entry.price || 0) / Object.keys(entry.userCheckedIds).filter(key => entry.userCheckedIds[key]).length
+    //     }
+    //     return sum
+    //   }, 0)
+    //   let userOwns = 0
+    //   // TODO: must be changed to id and the same on the backend
+    //   // TODO: consider adding a new filed something like createdByName so we do not have to lookup
+    //   if (sheet.createdBy === user.name) {
+    //     userOwns = sheet.entries.reduce((sum, entry) => {
+    //       sum += entry.price || 0
+    //       return sum
+    //     }, 0)
+    //   }
+    //   memo[sheet._id] = userOwns - userOwes
+    //   memo.total += userOwns - userOwes
+    //   return memo
+    // }, { total: 0 })
 
-      const items = this.state.sheets.map((sheet, index) => {
+    const items = (this.globalState.get("sheets") || []).map((sheet, index) => {
       const date = new Date(sheet.createdAt)
       const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
       const niceDate = `${months[date.getMonth()]} ${date.getDate()}`
@@ -88,13 +86,13 @@ export default class ExpenseSheetList extends React.Component {
         <Link
           className="pay-balance-btn"
           to={{
-            pathname: `/balances/${this.groupId}`,
+            pathname: `/balances/${this.props.groupId}`,
           }}
         >Pay Balance</Link>
         <h2>Sheets</h2>
         <ul className="expense-sheet-list">
           {items}
-          <li>Total: {summary.total > 0 ? "you own" : "you owe"} $ {Math.abs(summary.total)}</li>
+          {/*<li>Total: {summary.total > 0 ? "you own" : "you owe"} $ {Math.abs(summary.total)}</li>*/}
         </ul>
         <button onClick={this.addNewExpenseSheet.bind(this)}>Add New One</button>
       </div>
--- client/src/components/userSummary.jsx
+++ client/src/components/userSummary.jsx
@@ -2,14 +2,32 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { FaChevronDown } from "react-icons/fa"
 
-export default class GroupDropDown extends React.Component {
+import Spinner from "./spinner.jsx"
+
+export default class UserSummary extends React.Component {
   constructor(props) {
     super(props)
   }
 
   render() {
+    const sheets = this.globalState.get("sheets") || []
+    console.log(sheets)
+    const userId = JSON.parse(window.localStorage.getItem("user"))._id
+    const memeberSummary = sheets.reduce((memo, sheet) => {
+      //TODO: filter only sheets that all users marked as completed
+      const userOwes = sheet.usersPaidIds[userId] ? 0 : sheet.entries.reduce((sum, entry) => {
+        if (entry.userCheckedIds[userId]) {
+          sum += (entry.price || 0) / Object.keys(entry.userCheckedIds).filter(key => entry.userCheckedIds[key]).length
+        }
+        return sum
+      }, 0)
+      memo[sheet.createdBy] = userOwes
+      return memo
+    }, { })
+    console.log(memeberSummary)
     return (
       <div className="user-summary">
+        <Spinner />
       </div>
     )
   }
