20b549e36fcbd9fbc629f28a15e183060c32c604
Tim Nimets <tymnim@gmail.com>
2021-04-19 15:10:28 -0400

    fixed issue when delete entry would remove multiple entries

--- server/routes/deleteEntry.js
+++ server/routes/deleteEntry.js
@@ -29,7 +29,26 @@ exports.deleteEntry = {
         res.end()
         return
       }
-      await global.db.collection("sheets").updateOne({ _id: doc._id }, { $pull: { entries: entry } })
+      // HACK:
+      // Working with arrays, mongodb will not let you remove a single element whith non-unique values.
+      // If $pull is used and there're other documents with the same values it will remove all of them instead of one.
+      // See 
+      //    https://jira.mongodb.org/browse/SERVER-1014
+      //
+      // Atomic way of doing this would be deleting the entry with JS here and do $set in for the new array
+      // ```
+      //    doc.entries.splice(req.params.index, 1)
+      //    await global.db.collection("sheets").updateOne({ _id: doc._id }, { $set: { entries: doc.entries } })
+      // ```
+      // This might be more correct way of doing this (less network intensive, atomic, but more memory and more processor intensive)
+      // This might be implemented in the future. It depends what server and database server we'll get and what's the usage of entries
+      //
+      // For now let's stick with non-atomic one for now.
+      // Downside would be that there's a race condition possible that someone queries the data after it's been $unset and before $pull
+      // 
+      // I am not 100% sure, but another solution might be use of transactions
+      await global.db.collection("sheets").updateOne({ _id: doc._id }, { $unset: { [`entries.${req.params.index}`]: true } }),
+      await global.db.collection("sheets").updateOne({ _id: doc._id }, { $pull: { entries: null } })
       res.status(200)
       res.send({ message: "Deleted Entry Successfully!" })
       res.end()
